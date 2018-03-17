197a8755b7a3f8a30c56dcde193c47e021598c69
Erick Contreras <ericon99sdb@gmail.com>
2018-03-17 08:11:29 -0600

    GGG

---
+++ Lab3EDI_1C18/Lab3EDI_1C18/DBContext/DefaultConnection.cs
@@ -0,0 +1,47 @@
+﻿using Lab3EDI_1C18.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TDA.Clases;
+
+namespace Lab3EDI_1C18.DBContext
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+        ArbolAVL<Partido, int> arbolNoPartido = new ArbolAVL<Partido, int>();
+        List<Partido> listaNoPartido = new List<Partido>();
+
+        ArbolAVL<Partido, DateTime> arbolFechaPartido = new ArbolAVL<Partido, DateTime>();
+        List<Partido> listaFechaPartido = new List<Partido>();
+
+
+        public int IDActual { get; set; }
+
+        private DefaultConnection()
+        {
+            IDActual = 0;
+        }
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
