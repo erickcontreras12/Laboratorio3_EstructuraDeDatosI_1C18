52323f8aebbae79984204f0b3d984eddd03909e2
Erick Contreras <ericon99sdb@gmail.com>
2018-03-17 09:40:39 -0600

    Agregando insercion

---
+++ Lab3EDI_1C18/Lab3EDI_1C18/Controllers/PartidoController.cs
@@ -0,0 +1,148 @@
+﻿using Lab3EDI_1C18.DBContext;
+using Lab3EDI_1C18.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDA.Clases;
+
+namespace Lab3EDI_1C18.Controllers
+{
+    public class PartidoController : Controller
+    {
+        DefaultConnection db = DefaultConnection.getInstance;
+        // GET: Partido
+        public ActionResult Index()
+        {
+            return View(db.listaPartido.ToList());
+        }
+
+        public ActionResult IndexPorFecha()
+        {
+            return View(db.listaFechaPartido.ToList());
+        }
+
+        public ActionResult IndexPorNumero()
+        {
+            return View(db.listaNoPartido.ToList());
+        }
+
+        // GET: Partido/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Partido/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Partido/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "noPartido,FechaPartido,Grupo,Pais1,Pais2,Estadio")] Partido partido)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.arbolFechaPartido.FuncionObtenerLlave = ObtenerFecha;
+                db.arbolFechaPartido.FuncionCompararLlave = CompararFecha;
+                db.arbolNoPartido.FuncionObtenerLlave = ObtenerNumero;
+                db.arbolNoPartido.FuncionCompararLlave = CompararNumero;
+
+                db.listaFechaPartido.Clear();
+                db.listaNoPartido.Clear();
+
+                db.arbolFechaPartido.Insertar(partido);
+                db.arbolNoPartido.Insertar(partido);
+                db.listaPartido.Add(partido);
+
+                db.arbolFechaPartido.EnOrden(RecorrerPartidoInFecha);
+                db.arbolNoPartido.EnOrden(RecorrerPartidoInNumero);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+
+        // GET: Partido/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Partido/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Partido/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Partido/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public void RecorrerPartidoInFecha(Nodo<Partido> actual)
+        {
+            db.listaFechaPartido.Add(actual.valor);
+        }
+
+        public void RecorrerPartidoInNumero(Nodo<Partido> actual)
+        {
+            db.listaNoPartido.Add(actual.valor);
+        }
+
+        public static int ObtenerNumero(Partido dato)
+        {
+            return dato.noPartido;
+        }
+
+        public static DateTime ObtenerFecha(Partido dato)
+        {
+            return dato.FechaPartido;
+        }
+
+        public static int CompararNumero(int actual, int nuevo)
+        {
+            return actual.CompareTo(nuevo);
+        }
+
+        public static int CompararFecha(DateTime actual, DateTime nuevo)
+        {
+            return actual.CompareTo(nuevo);
+        }
+    }
+}
--- Lab3EDI_1C18/Lab3EDI_1C18/DBContext/DefaultConnection.cs
+++ Lab3EDI_1C18/Lab3EDI_1C18/DBContext/DefaultConnection.cs
@@ -12,11 +12,13 @@ namespace Lab3EDI_1C18.DBContext
         private static volatile DefaultConnection Instance;
         private static object syncRoot = new Object();
 
-        ArbolAVL<Partido, int> arbolNoPartido = new ArbolAVL<Partido, int>();
-        List<Partido> listaNoPartido = new List<Partido>();
+        public List<Partido> listaPartido = new List<Partido>();
 
-        ArbolAVL<Partido, DateTime> arbolFechaPartido = new ArbolAVL<Partido, DateTime>();
-        List<Partido> listaFechaPartido = new List<Partido>();
+        public ArbolAVL<Partido, int> arbolNoPartido = new ArbolAVL<Partido, int>();
+        public List<Partido> listaNoPartido = new List<Partido>();
+
+        public ArbolAVL<Partido, DateTime> arbolFechaPartido = new ArbolAVL<Partido, DateTime>();
+        public List<Partido> listaFechaPartido = new List<Partido>();
 
 
         public int IDActual { get; set; }
